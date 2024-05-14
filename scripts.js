fc10be2617ce539fb89160bf61233c9818f31215
Netasouza <mel8689@gmail.com>
2024-05-14 12:33:37 -0300

    projeto mobile 0.0.1
    projeto mobile

---
+++ scripts.js
@@ -0,0 +1,30 @@
+body, button {
+    font-family: Arial, sans-serif;
+}
+
+.container {
+    text-align: center;
+    padding-top: 50px;
+}
+
+#page-content {
+    margin-bottom: 20px;
+    font-size: 20px;
+}
+
+button {
+    margin: 5px;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+}
+
+.dark-theme {
+    background-color: #333;
+    color: white;
+}
+
+.light-theme {
+    background-color: #fff;
+    color: #333;
+}
